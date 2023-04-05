550f44805cba0ece8134559cc50268b81b441ed6
Ryan Pairitz <ryn.pair@me.com>
2023-04-04 20:18:35 -0400

    included downloads and init firebase

--- src/Components/Logos/Downloads.js
+++ src/Components/Logos/Downloads.js
@@ -1,7 +1,25 @@
 import { forwardRef } from "react";
 import SectionDivider from "../SectionDivider";
+import DownloadBrandmark from "./DownloadBrandmark";
+import DownloadLight from "./vinal-brandmark_light.svg";
+import DownloadDark from "./vinal-brandmark_dark.svg";
 
 const Downloads = forwardRef((props, downloadsRef) => {
+    const Downloads = [
+        {
+            id: "light",
+            fill: "var(--white)",
+            description: "For dark-colored backgrounds",
+            file: DownloadLight
+        },
+        {
+            id: "dark",
+            fill: "var(--black-carbon)",
+            description: "For light-colored backgrounds",
+            file: DownloadDark
+        }
+    ]
+
     return (
         <section
             ref={downloadsRef}
@@ -13,7 +31,16 @@ const Downloads = forwardRef((props, downloadsRef) => {
                 <p>
                     Below are official downloads for you to include the simplified Vinal brandmark on your own webpage, app, or link-in-bio tool, alongside other social icons.
                 </p>
-                <div>
+                <div className="card-container">
+                    {Downloads.map((download, index) => (
+                        <a href={download.file} download={`vinal-brandmark_${download.id}.svg`}
+                            className="download-container" key={index}>
+                            <DownloadBrandmark className="interactive-logo download" fill={download.fill} />
+                            <p>
+                                {download.description}
+                            </p>
+                        </a>
+                    ))}
                 </div>
             </div>
         </section>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYOGz181sjMIxjOYYeyzTNO8jDdPa850M",
+  authDomain: "vinal-brand-guide.firebaseapp.com",
+  projectId: "vinal-brand-guide",
+  storageBucket: "vinal-brand-guide.appspot.com",
+  messagingSenderId: "1049235880946",
+  appId: "1:1049235880946:web:93020f2f135e75ceecc96b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
